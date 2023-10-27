8d4ee02df96ec3e154929d6f5913aa98d0bc6978
div-styl <gouhmada@gmail.com>
2023-10-27 12:32:16 +0100

    load function makes the api more faster

--- src/routes/Definition/[word]/+page.js
+++ src/routes/Definition/[word]/+page.js
@@ -1,16 +1,14 @@
-/** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
-	const { word } = params;
-	let define = '';
+	let { word } = params;
 
 	// Make the API request here
-	const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-	const data = await response.json();
+	let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+	let data = await response.json();
 
-	define = data[0]?.meanings[1]?.definitions[0]?.definition;
-	console.log(JSON.stringify(define, null, 4));
+	let define = data[0]?.meanings[1]?.definitions[0]?.definition;
+	console.log('define: ' + JSON.stringify(define, null, 4));
 
-	return {
-		props: define
-	};
+	// Return the data as a prop
+	return { define };
 }
+// #TODO: fix the props
--- src/routes/Definition/[word]/+page.svelte
+++ src/routes/Definition/[word]/+page.svelte
@@ -1,17 +1,15 @@
 <script>
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
 	let word = $page.params.word;
 	let pagetitle = `Definition of ${word}`;
 	let description = `Definition page of  ${word}`;
-	let define = '';
-	// call the api
-	onMount(async () => {
-		let url = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-		let data = await url.json();
-		define = data[0]?.meanings[1]?.definitions[0]?.definition;
-		console.log(JSON.stringify(define, null, 4));
-	});
+
+	export let data;
+	$: define = data.define;
+
+	console.log('data:', data);
+	console.log('word:', word);
+	console.log('define:', define);
 </script>
 
 <svelte:head>
@@ -27,3 +25,4 @@
 		<h2 class="text-2xl flex justify-center text-blue-600 font-black">No definition found</h2>
 	{/if}
 </main>
+<!-- #TODO: Add the definition it is not working -->
