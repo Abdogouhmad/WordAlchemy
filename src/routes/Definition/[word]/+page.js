95661ee47bf106eb78dc47f60ca45da0f0a7e6ff
div-styl <gouhmada@gmail.com>
2023-10-28 15:11:36 +0100

    new parms :rocket:

--- src/routes/Definition/[word]/+page.js
+++ src/routes/Definition/[word]/+page.js
@@ -6,9 +6,16 @@ export async function load({ fetch, params }) {
 	let data = await response.json();
 
 	let define = data[0]?.meanings[1]?.definitions[0]?.definition;
-	console.log('define: ' + JSON.stringify(define, null, 4));
+	let partofspeach = data[0].meanings[1].partOfSpeech;
+	let phonetics = data[0]?.phonetic;
+	let phoneaudio = data[0]?.phonetics[0]?.audio;
+	console.log('define from server: ' + JSON.stringify(define, null, 4));
 
 	// Return the data as a prop
-	return { define };
+	return {
+		def: define,
+		partsp: partofspeach,
+		phoni: phonetics,
+		phoniaudio: phoneaudio
+	};
 }
-// #TODO: fix the props
--- src/routes/Definition/[word]/+page.svelte
+++ src/routes/Definition/[word]/+page.svelte
@@ -1,15 +1,17 @@
 <script>
 	import { page } from '$app/stores';
+	import { b } from 'vitest/dist/reporters-5f784f42';
 	let word = $page.params.word;
 	let pagetitle = `Definition of ${word}`;
 	let description = `Definition page of  ${word}`;
 
 	export let data;
-	$: define = data.define;
+	$: define = data.def.define;// #TODO: fix the props
+	$: phontics = data.phoni.phonetics;
+	$: partspch = data.partsp.partofspeach;
+	$: phoniaudio = data.phoniaudio.phoneaudio;
 
-	console.log('data:', data);
-	console.log('word:', word);
-	console.log('define:', define);
+	console.log('data from page:', JSON.stringify(data, null, 4));
 </script>
 
 <svelte:head>
@@ -17,12 +19,29 @@
 	<meta name="description" content={description} />
 </svelte:head>
 
-<main class="pt-20">
-	<h1 class="text-6xl flex justify-center text-blue-600 font-black">{word}</h1>
+<main class="pt-2">
+	<!-- * header -->
+	<div class="text-black font-thin justify-center flex text-justify text-sm">
+		<p>
+			Definition of <b class="font-bold text-gray-700">{word}</b> From The WordAlchemy's Dictionary
+		</p>
+	</div>
+	<!-- * top container -->
 	{#if define}
-		<h2 class="text-2xl flex justify-center text-blue-600 font-black">{define}</h2>
+		<div class="flex justify-center pt-5">
+			<!-- align to left -->
+			<div class="flex justify-start">
+				<h1 class="text-5xl font-bold text-blue-600">{word} <em class="font-normal text-2xl text-gray-700 ">{partspch}</em></h1>
+			</div>
+		</div>
+		<div class="flex justify-center pt-5 border border-t border-black">
+			<!-- align to left -->
+			<p class="text-black text-3xl font-bold">Hello world</p>
+		</div>	
 	{:else}
-		<h2 class="text-2xl flex justify-center text-blue-600 font-black">No definition found</h2>
+		<h1 class="text-center justify-center flex text-6xl text-red-600">
+			No definition for the word: <b>{word}</b>
+		</h1>
 	{/if}
 </main>
 <!-- #TODO: Add the definition it is not working -->
