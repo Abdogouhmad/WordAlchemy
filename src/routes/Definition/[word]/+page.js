b194fdead22cfac6b0f92b7c8b29a1403e2d983a
div-styl <gouhmada@gmail.com>
2023-11-05 23:10:09 +0100

    Update!

--- src/routes/Definition/[word]/+page.js
+++ src/routes/Definition/[word]/+page.js
@@ -4,21 +4,15 @@ function Meaning(data) {
 		let worddictionary = dataparesed[0]?.word;
 		let partofspeech = dataparesed[0]?.meanings[0].partOfSpeech;
 		let difine0 = dataparesed[0]?.meanings[0].definitions[0].definition;
-		let difine1 = dataparesed[0]?.meanings[0].definitions[1].definition;
-		let difine2 = dataparesed[0]?.meanings[0].definitions[2].definition;
+
 		let example0 = dataparesed[0]?.meanings[0].definitions[0].example;
-		let example1 = dataparesed[0]?.meanings[0].definitions[1].example;
-		let example2 = dataparesed[0]?.meanings[0].definitions[2].example;
+
 
 		return {
 			partofspeech,
 			worddictionary,
 			difine0,
-			difine1,
-			difine2,
 			example0,
-			example1,
-			example2,
 		}
 	} else {
 		throw new Error('NO DATA NIGGA');
--- src/routes/Definition/[word]/+page.svelte
+++ src/routes/Definition/[word]/+page.svelte
@@ -5,23 +5,14 @@
 	export let data;
 	$: partspch = data?.dataparesed?.partofspeech;
 	$: wordifne = data?.dataparesed?.worddictionary;
-	$: define = [
-		data?.dataparesed?.difine0 ||
-			data?.dataparesed?.difine1 ||
-			data?.dataparesed?.difine2 ||
-			'no definition found'
-	].filter(Boolean);
-	$: examples = [
-		data?.dataparesed?.example0 ||
-			data?.dataparesed?.example1 ||
-			data?.dataparesed?.example2 ||
-			'no example found'
-	].filter(Boolean);
+	$: define = data?.dataparesed?.difine0 || 'no definition found';
+	$: examples = data?.dataparesed?.example0 || 'no example found';
+	$: {
+		console.log(data);
+	}
 
 	let pagetitle = `Definition of ${wordifne}`;
 	let description = `Definition page of  ${wordifne}`;
-
-	console.log('data from page:', JSON.stringify(data, null, 4));
 </script>
 
 <svelte:head>
@@ -44,22 +35,20 @@
 				{wordifne} <em class="font-normal text-base text-blue-600">{partspch}</em>
 			</h1>
 			<div class="pt-5 flex flex-row justify-between">
-				<h1 class="font-bold md:text-2xl italic text-base mx-3">{partspch}</h1>
-				<hr class="w-[15cm] mx-2 border-1 md:my-5 my-5" />
+				<h1 class="font-bold md:text-2xl italic text-lg">{partspch}</h1>
+				<hr class="w-[10cm] md:w-[20cm] mx-2 border-1 md:my-5 my-3" />
 			</div>
 			<!-- !meaning section -->
 			<div>
-				<h2 class="md:text-2xl text-base font-semibold italic">Meaning</h2>
+				<h2 class="md:text-2xl pt-4 text-base font-semibold italic">Meaning</h2>
 				<ul class="pt-5 text-justify text-sm md:text-base font-medium list-disc list-inside">
 					<li class="text-base md:text-center leading-relaxed">
-						{#each define as def}
-							{def}
+						{#if define}
+							{define}
 							{#if examples && examples.length > 0}
-								{#each examples as ex}
-									<q class="block text-sm text-[#979797] italic pt-2">{ex}</q>
-								{/each}
+								<q class="block text-sm text-[#979797] italic pt-2">{examples}</q>
 							{/if}
-						{/each}
+						{/if}
 					</li>
 				</ul>
 			</div>
