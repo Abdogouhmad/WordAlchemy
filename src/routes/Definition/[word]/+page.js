c38c44b171cf0645adbe8f2b35e1ef336ecd07f4
div-styl <gouhmada@gmail.com>
2023-11-05 22:30:28 +0100

    :rocket: still working and try to figure it out

--- src/routes/Definition/[word]/+page.js
+++ src/routes/Definition/[word]/+page.js
@@ -1,26 +1,47 @@
+function Meaning(data) {
+	let dataparesed  = JSON.parse(JSON.stringify(data));
+	if (dataparesed !== undefined) {
+		let worddictionary = dataparesed[0]?.word;
+		let partofspeech = dataparesed[0]?.meanings[0].partOfSpeech;
+		let difine0 = dataparesed[0]?.meanings[0].definitions[0].definition;
+		let difine1 = dataparesed[0]?.meanings[0].definitions[1].definition;
+		let difine2 = dataparesed[0]?.meanings[0].definitions[2].definition;
+		let example0 = dataparesed[0]?.meanings[0].definitions[0].example;
+		let example1 = dataparesed[0]?.meanings[0].definitions[1].example;
+		let example2 = dataparesed[0]?.meanings[0].definitions[2].example;
+
+		return {
+			partofspeech,
+			worddictionary,
+			difine0,
+			difine1,
+			difine2,
+			example0,
+			example1,
+			example2,
+		}
+	} else {
+		throw new Error('NO DATA NIGGA');
+	}
+}
+
 export async function load({ fetch, params }) {
 	let { word } = params;
 
 	// Make the API request here
 	let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
 	let data = await response.json();
-	let dataparesed = JSON.parse(JSON.stringify(data));
-	let worddictionary = dataparesed[0]?.word;
-	let firstarr = dataparesed[0]?.meanings[0];
-	console.log(firstarr.meanings);
+	// let dataparesed = JSON.parse(JSON.stringify(data));
+	// let worddictionary = dataparesed[0]?.word;
+	// let firstarr = dataparesed[0]?.meanings[0];
+	// console.log(firstarr.meanings);
+	let dataparesed = Meaning(data)
 
 	return {
-		firstarr, 
-		worddictionary
+		dataparesed
+		// firstarr, 
+		// worddictionary
 	};
 }
 
-// let worddictionary = dataparesed[0]?.word;
-// let define = dataparesed[0]?.meanings[1]?.definitions[0]?.definition;
-// let partofspeach = dataparesed[0]?.meanings[1]?.partOfSpeech;
-// let phonetics = dataparesed[0]?.phonetic;
-// let phoneaudio = dataparesed[0]?.phonetics[6]?.audio;
-// console.log('define from server: ' + JSON.stringify(define, null, 4));
 
-// // Return the data as a prop
-// return { define, partofspeach, phonetics, phoneaudio, worddictionary };
