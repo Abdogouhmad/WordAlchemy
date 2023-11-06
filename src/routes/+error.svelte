daa9acdaad07eb66ad286fa17959ef1dd8014b25
div-styl <gouhmada@gmail.com>
2023-11-06 22:55:37 +0100

    Update!

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,26 +1,26 @@
 <script>
 	import { page } from '$app/stores';
-	// import { onMount } from 'svelte';
-	// import { Caterror } from '$lib';
-	// import lottie from 'lottie-web';
+	import { onMount } from 'svelte';
+	import { Caterror } from '$lib';
+	import lottie from 'lottie-web';
 	let errorpage = $page.error?.message;
 	const pageTitle = '404 Not found';
 	const pageDescription = 'Page not found on WordAlchemy';
-	// let lottieElement;
+	let lottieElement;
 
-	// onMount(() => {
-	// 	lottie.loadAnimation({
-	// 		container: lottieElement,
-	// 		loop: true,
-	// 		autoplay: true,
-	// 		animationData: Caterror
-	// 	});
+	onMount(() => {
+		lottie.loadAnimation({
+			container: lottieElement,
+			loop: true,
+			autoplay: true,
+			animationData: Caterror
+		});
 
-	// 	return () => {
-	// 		// You can destroy the animation if needed when the component is unmounted
-	// 		lottieElement.destroy();
-	// 	};
-	// });
+		return () => {
+			// You can destroy the animation if needed when the component is unmounted
+			lottieElement.destroy();
+		};
+	});
 </script>
 
 <svelte:head>
@@ -31,7 +31,7 @@
 <div class="flex items-center justify-center h-screen">
 	<div class="">
 		<div class="flex flex-col items-center">
-			<!-- <div class="md:h-[30vh] h-[200px] lg:h-[67vh] w-full" bind:this={lottieElement} /> -->
+			<div class="md:h-[30vh] h-[200px] lg:h-[67vh] w-full" bind:this={lottieElement} />
 			<h1 class="font-bold text-6xl text-blue-500 lg:text-8xl">{$page.status}</h1>
 
 			<h6 class="mb-2 text-2xl font-bold text-center text-black md:text-4xl lg:text-6xl">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,26 @@
 <script>
-	// import { onMount } from 'svelte';
-	// import { Catlookup } from '$lib';
-	// import lottie from 'lottie-web';
+	import { onMount } from 'svelte';
+	import { Catlookup } from '$lib';
+	import lottie from 'lottie-web';
 	let pageTitle = 'WordAlchemy';
 	let pageDescription = 'Welcome to WordAlchemy';
 
-	// let lottieElement;
+	let lottieElement;
 
-	// onMount(() => {
-	// 	// Load and display the Lottie animation
-	// 	const animation = lottie.loadAnimation({
-	// 		container: lottieElement,
-	// 		loop: true, // Set to true if you want the animation to loop
-	// 		autoplay: true, // Set to true if you want the animation to play automatically
-	// 		animationData: Catlookup // Replace with your Lottie animation data
-	// 	});
+	onMount(() => {
+		// Load and display the Lottie animation
+		const animation = lottie.loadAnimation({
+			container: lottieElement,
+			loop: true, // Set to true if you want the animation to loop
+			autoplay: true, // Set to true if you want the animation to play automatically
+			animationData: Catlookup // Replace with your Lottie animation data
+		});
 
-	// 	return () => {
-	// 		// Cleanup when the component is unmounted
-	// 		animation.destroy();
-	// 	};
-	// });
+		return () => {
+			// Cleanup when the component is unmounted
+			animation.destroy();
+		};
+	});
 </script>
 
 <svelte:head>
@@ -29,7 +29,7 @@
 </svelte:head>
 
 <section class="flex flex-col justify-center items-center">
-	<!-- <div bind:this={lottieElement} class="md:h-[30vh] h-[200px] lg:h-[67vh]" /> -->
+	<div bind:this={lottieElement} class="md:h-[30vh] h-[200px] lg:h-[67vh]" />
 	<h1 class="md:text-5xl text-2xl pt-5 font-mono">Welcome to WordAlchemy</h1>
 	<p class="md:text-3xl text-base font-light p-3 pt-3 text-center">Hello I know you are tired of ads in
 		websites, well you have just discovered WordAlchemy!!!</p>
