6c91f757185596cf850a59644842f363cebbcb9c
div-styl <gouhmada@gmail.com>
2023-11-10 00:44:06 +0100

    Update Searchbar styling 🤞

--- src/lib/components/Body/Searchbar.svelte
+++ src/lib/components/Body/Searchbar.svelte
@@ -21,7 +21,7 @@
 			border-blue-300 rounded-md bg-gray-50
 			focus:ring-blue-500
 			dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-500 dark:text-white
-			dark:focus:border-blue-500"
+			dark:hover:border-blue-500 "
 			placeholder="Search for a word..."
 			required
 		/>
@@ -29,7 +29,7 @@
 			type="submit"
 			class="text-white absolute right-3.5 bottom-3.5 bg-blue-700 hover:bg-blue-800
             focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2
-            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button
+            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:border-blue-800">Search</button
 		>
 	</div>
 </form>
