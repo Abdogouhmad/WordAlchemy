bf5f6f05d4bb4497c64a07073359ca5f4337f09c
div-styl <gouhmada@gmail.com>
2023-11-11 14:14:34 +0100

    Add Lottie animation and multiple dictionary sources to Definition page 🚀 😸

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,27 @@
 <script>
 	import { onMount } from 'svelte';
 	import { Catlookup } from '$lib';
-	import lottie from 'lottie-web';
 	let pageTitle = 'WordAlchemy';
 	let pageDescription = 'Welcome to WordAlchemy';
 
 	let lottieElement;
 
 	onMount(() => {
-		// Load and display the Lottie animation
-		const animation = lottie.loadAnimation({
-			container: lottieElement,
-			loop: true, // Set to true if you want the animation to loop
-			autoplay: true, // Set to true if you want the animation to play automatically
-			animationData: Catlookup // Replace with your Lottie animation data
-		});
+    import('lottie-web').then(lottie => {
+        // Load and display the Lottie animation
+        const animation = lottie.loadAnimation({
+            container: lottieElement,
+            loop: true, // Set to true if you want the animation to loop
+            autoplay: true, // Set to true if you want the animation to play automatically
+            animationData: Catlookup // Replace with your Lottie animation data
+        });
 
-		return () => {
-			// Cleanup when the component is unmounted
-			animation.destroy();
-		};
-	});
+        return () => {
+            // Cleanup when the component is unmounted
+            animation.destroy();
+        };
+    });
+});
 </script>
 
 <svelte:head>
--- src/routes/Definition/[word]/+page.svelte
+++ src/routes/Definition/[word]/+page.svelte
@@ -5,12 +5,45 @@
 	$: phonetic = data?.phonetics;
 	$: wordefine = data?.word;
 
-	let wordsearch, pagetitle, description;
+	let wordsearch, pagetitle, description, dictionaries;
 
 	$: {
 		wordsearch = wordefine;
 		pagetitle = `WordAlchemy | Definition of ${wordsearch}`;
 		description = `Definition page of ${wordsearch}`;
+		dictionaries = [
+			{
+				name: 'Oxford',
+				imageurl:
+					'https://www.oxfordlearnersdictionaries.com/us/external/images/home_2020/OLD_home_productsOALD.png?version=2.3.40',
+				wordlink: `https://www.oxfordlearnersdictionaries.com/definition/english/${wordsearch}`
+			},
+			{
+				name: 'Merriam-Webster',
+				imageurl:'https://th.bing.com/th/id/R.9ec2334023d1b77d96e817fec0867e92?rik=1o6%2f82j%2b732iNg&pid=ImgRaw&r=0',
+				wordlink: `https://www.merriam-webster.com/dictionary/${wordsearch}`
+			},
+			{
+				name: 'Collins',
+				imageurl: 'https://th.bing.com/th/id/R.4b6538cbae2736939980764d3ead4815?rik=nbJverqn8NEXmQ&pid=ImgRaw&r=0',
+				wordlink: `https://www.collinsdictionary.com/dictionary/english/${wordsearch}`
+			},
+			{
+				name: 'Wikipedia',
+				imageurl: 'https://th.bing.com/th/id/R.e156c235334d36c76030c3724a3a091e?rik=z2fOIIhlHdNnlA&pid=ImgRaw&r=0',
+				wordlink: `https://en.wikipedia.org/wiki/${wordsearch}`
+			},
+			{
+				name: 'Dictionary',
+				imageurl: 'https://www.dictionary.com/e/wp-content/uploads/2016/09/20200825_genericImage_Dcom_800x800-500x500.png',
+				wordlink: `https://www.dictionary.com/browse/${wordsearch}`
+			},
+			{
+				name: 'Cambridge',
+				imageurl: 'https://beyondidiomas.com/wp-content/uploads/2019/03/cambridge_dictionary.jpg',
+				wordlink: `https://dictionary.cambridge.org/dictionary/english/${wordsearch}`
+			}
+		];
 	}
 </script>
 
@@ -47,13 +80,27 @@
 							<li class="text-base my-2 text-start leading-relaxed">
 								{def.definition}
 								{#if def.example}
-									<p class="block text-base text-blue-700 dark:text-blue-600/60 italic pt-3">{def.example}</p>
+									<p class="block text-base text-blue-700 dark:text-blue-600/60 italic pt-3">
+										{def.example}
+									</p>
 								{/if}
 							</li>
 						{/each}
 					</ul>
 				</div>
 			{/each}
+			<!-- sources -->
+			<div class="pt-5 flex flex-row justify-between">
+				<h1 class="font-bold md:text-2xl italic text-xl">Sources</h1>
+				<hr class="w-full md:w-[25cm] mx-2 border-1 border-blue-600 my-4" />
+			</div>
+			<div class="flex flex-row pt-3 space-x-3">
+				{#each dictionaries as { name, imageurl, wordlink }}
+					<a href={wordlink} target="_blank">
+						<img src={imageurl} alt={name} class="w-10 h-10 rounded-full" />
+					</a>
+				{/each}
+			</div>
 		</div>
 	{:else}
 		<div class="pt-10">
