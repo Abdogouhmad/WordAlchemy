d784f2dc4f01b6e03615cf9bd8c09418c04fc706
div-styl <gouhmada@gmail.com>
2023-11-17 00:04:21 +0100

    inject analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script>
 	/** @type {import('./$types').LayoutData} */
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
 	import '../app.css';
 	import { Navbar } from '$lib';
 	import { DEngine } from '$lib';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <header>
