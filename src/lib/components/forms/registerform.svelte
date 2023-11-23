3eea181799dba7561ae478e16440dbb3ecf2ad19
div-styl <gouhmada@gmail.com>
2023-11-23 22:05:22 +0100

    showding the singup page :art:

--- src/lib/components/forms/registerform.svelte
+++ src/lib/components/forms/registerform.svelte
@@ -8,7 +8,7 @@
 		WordAlchemy
 	</a>
 	<div
-		class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+		class="w-full bg-white rounded-lg shadow-lg shadow-blue-400/50 dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
 	>
 		<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
 			<h1
