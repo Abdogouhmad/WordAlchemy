55291cf5c6df16f354dd9df624c1fb79debce25d
div-styl <gouhmada@gmail.com>
2023-11-24 20:30:39 +0100

    Add theme toggle component to mobile header

--- src/lib/components/Header/Mobileheader.svelte
+++ src/lib/components/Header/Mobileheader.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { Hum } from '$lib';
 	import Icon from '$lib/assets/Icon.svg';
+	import Toggletheme from './Theme/selectheme.svelte';
 
 	let isNavOpen = false;
 
@@ -33,7 +34,7 @@
 </script>
 
 <div class="flex items-center justify-between md:hidden p-3">
-	<a href="/" class="flex items-start justify-start">
+	<a href="/" class="flex items-start justify-start" on:click={closeNavBar}>
 		<img src={Icon} class="h-7 w-7" alt="WordAlchemy Logo" />
 		<span class="text-2xl font-semibold mx-2">WordAlchemy</span>
 	</a>
@@ -42,8 +43,8 @@
 	</button>
 
 	<div
-		class={`bg-gray-900 transition-transform ${
-			isNavOpen ? 'translate-x-0' : '-translate-x-full'
+		class={`bg-white dark:bg-black transition-transform ${
+			isNavOpen ? 'showFromRight' : '-translate-x-full'
 		} fixed w-screen h-screen top-0 left-0 flex flex-col justify-center items-center md:hidden z-[999]`}
 	>
 		<ul class="text-2xl flex flex-col space-y-5 font-medium">
@@ -52,12 +53,16 @@
 					<a
 						href={item.href}
 						on:click={closeNavBar}
-						class="text-white hover:text-blue-600 hover:underline"
+						class=" text-lg hover:text-blue-600 hover:underline"
 					>
 						{item.name}
 					</a>
 				</li>
 			{/each}
 		</ul>
+		<div class="pt-3">
+			<Toggletheme />
+		</div>
 	</div>
+
 </div>
--- src/lib/components/Header/Theme/selectheme.svelte
+++ src/lib/components/Header/Theme/selectheme.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
+
 	let darkMode = false;
 	let darkModeReady = false;
 
@@ -12,32 +13,31 @@
 </script>
 
 {#if darkModeReady}
-	<div class="h-6 toggle " in:fade={{ duration: 800 }}>
+	<div class="h-6 toggle" in:fade={{ duration: 800 }}>
 		<input
 			type="checkbox"
 			id="toggle"
 			bind:checked={darkMode}
-			on:change={() => document.documentElement.classList.toggle('dark')}
+			on:change={() => {
+				document.documentElement.classList.toggle('dark', darkMode);
+			}}
 		/>
 		<label id="darkicon" title="Toggle dark mode" for="toggle" data-dark={darkMode ? '🌙' : '🌞'} />
 	</div>
 {/if}
 
-<style style lang="postcss">
+<style lang="postcss">
 	.toggle input[type='checkbox'] {
 		display: none;
 	}
 
 	.toggle label {
-		@apply inline-block cursor-pointer relative transition-all ease-in-out 
-		duration-300 w-12 h-6 rounded-3xl 
-		border border-solid 
-		border-blue-700 bg-yellow-100;
+		@apply inline-block cursor-pointer relative transition-all ease-in-out duration-300 w-12 h-6 rounded-3xl border border-solid border-blue-700 bg-yellow-100;
 	}
 
 	.toggle label::after {
 		content: attr(data-dark);
-		@apply flex items-center justify-center rounded-full cursor-pointer absolute top-px left-px transition-all ease-in-out duration-300 w-5 h-5 bg-transparent align-middle;
+		@apply flex items-center justify-center rounded-full cursor-pointer absolute top-0 left-0 transition-all ease-in-out duration-300 w-5 h-5 bg-transparent;
 	}
 
 	.toggle input[type='checkbox']:checked ~ label {
@@ -48,5 +48,3 @@
 		transform: translateX(24px);
 	}
 </style>
-
-<!-- bg-gray-500 -->
