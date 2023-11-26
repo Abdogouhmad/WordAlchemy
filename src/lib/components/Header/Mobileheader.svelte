8b8b009c8ca33e1760344b4664dab61ddded2dd5
div-styl <gouhmada@gmail.com>
2023-11-26 01:09:03 +0100

    Fix mobile header navigation button behavior

--- src/lib/components/Header/Mobileheader.svelte
+++ src/lib/components/Header/Mobileheader.svelte
@@ -5,14 +5,24 @@
 	import Cbutton from './navbutton.svelte';
 	import { Logoutform } from '$lib';
 
-	let isNavOpen = false;
+	export const route = [
+		{
+			name: 'Log in',
+			href: '/auth/login'
+		},
+		{
+			name: 'Sing Up',
+			href: '/auth/register'
+		}
+	];
+	export let isNavOpen = false;
 	export let data;
 
 	const toggleNavBar = () => {
 		isNavOpen = !isNavOpen;
 	};
 
-	const closeNavBar = () => {
+	export const closeNavBar = () => {
 		isNavOpen = false;
 	};
 </script>
@@ -22,28 +32,44 @@
 		<img src={Icon} class="h-7 w-7" alt="WordAlchemy Logo" />
 		<span class="text-2xl font-semibold mx-2">WordAlchemy</span>
 	</a>
+
 	<button on:click={toggleNavBar} class="flex flex-col justify-between items-center md:hidden">
 		<Hum open={isNavOpen} />
 	</button>
 
 	<div
-		class={`bg-white dark:bg-black transition-transform ${
+		class={`bg-white dark:bg-black transition ${
 			isNavOpen ? 'showFromRight' : '-translate-x-full'
-		} fixed w-screen h-screen top-0 left-0 flex flex-col justify-center items-center md:hidden z-[999]`}
+		} fixed w-screen h-screen top-0 left-0 flex flex-col 
+        justify-center items-center md:hidden z-[999]`}
 	>
 		<ul class="text-2xl flex flex-col space-y-5 font-medium">
 			{#if data.session}
-				<a href="/" class="hover:text-blue-600 hover:underline pt-1 font-bold text-2xl">Home</a>
-				<a href="/auth/callback" class="hover:text-blue-600 hover:underline pt-1 font-bold text-2xl"
-					>Profile</a
+				<a
+					href="/"
+					on:click={closeNavBar}
+					class="hover:text-blue-600 hover:underline pt-1 font-bold text-2xl">Home</a
 				>
-				<Logoutform />
+				<a
+					href="/auth/callback"
+					on:click={closeNavBar}
+					class="hover:text-blue-600 hover:underline pt-1 font-bold text-2xl">Profile</a
+				>
+				<Logoutform on:click={closeNavBar} />
 			{:else}
-				<a href="/" class="hover:text-blue-600 hover:underline pt-1 font-bold text-2xl">Home</a>
-				<a href="/auth/callback" class="hover:text-blue-600 hover:underline pt-1 font-bold text-2xl"
-					>Profile</a
+				<a
+					href="/"
+					on:click={closeNavBar}
+					class="hover:text-blue-600 hover:underline pt-1 font-bold text-2xl">Home</a
 				>
-				<Cbutton />
+				{#each route as { name, href }}
+					<button
+						on:click={closeNavBar}
+						class="dark:bg-blue-500/30 bg-blue-500/60 hover:bg-blue-700 dark:hover:bg-blue-700 text-base font-bold py-2 px-2 rounded"
+					>
+						<a {href}>{name}</a>
+					</button>
+				{/each}
 			{/if}
 		</ul>
 		<div class="pt-3">
--- src/lib/components/Header/navbutton.svelte
+++ src/lib/components/Header/navbutton.svelte
@@ -1,4 +1,9 @@
 <script>
+	export let isNavOpen = false;
+	export const closeNavBar = () => {
+		isNavOpen = false;
+		console.log('fromnavbutton: ', isNavOpen);
+	};
 	export const route = [
 		{
 			name: 'Log in',
@@ -12,7 +17,7 @@
 </script>
 
 {#each route as { name, href }}
-	<button
+	<button on:click={closeNavBar}
 		class="dark:bg-blue-500/30 bg-blue-500/60 hover:bg-blue-700 dark:hover:bg-blue-700 text-base font-bold py-2 px-2 rounded"
 	>
 		<a {href}>{name}</a>
