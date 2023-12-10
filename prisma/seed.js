2ecf8cd6f5473a3a6cf2ebe41a8e7e22a3e851df
div-styl <gouhmada@gmail.com>
2023-12-10 03:33:21 +0100

    Add Prisma seed script and disable explicit any rule in ESLint 🚀

---
+++ prisma/seed.js
@@ -0,0 +1,21 @@
+import { db } from '$lib/db';
+
+async function main() {
+	const user = await db.user.create({
+		data: {
+			email: 'lifewithprisma@gmail.com',
+			password: '1990cool'
+		}
+	});
+    console.log(`Created user with id: ${user.id}`);
+}
+
+main()
+    .then (async () => {
+        await db.$disconnect()
+    })
+    .catch (async (e) => {
+        console.error(e)
+        await db.$disconnect()
+        process.exit(1)
+    })
