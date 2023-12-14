a224b80a0167db9dd1afd71786c4ea6fef130747
adil mae <itsadilhadid@gmail.com>
2023-12-14 17:05:35 +0100

    -

---
+++ src/routes/collection/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	let isOpen = false;
+
+	const responsive = 'md:w-[500px] md:mx-auto lg:w-1/2 lg:mx-auto rounded-lg';
+</script>
+
+<button
+	on:click={() => (isOpen = !isOpen)}
+	class="w-full {responsive} flex justify-center border border-red-600"
+>
+	collection
+</button>
+
+{#if isOpen}
+	<ul class="space-y-2 flex flex-col">
+		<li class="{responsive} px-4 mt-4 rounded-lg flex justify-between border border-blue-600">
+			<span>word 1</span>
+
+			<div>
+				<button>expand</button>
+			</div>
+		</li>
+		<li class="{responsive} rounded-lg flex justify-center border border-blue-600">word 2</li>
+		<li class="{responsive} rounded-lg flex justify-center border border-blue-600">word 3</li>
+	</ul>
+{/if}
--- svelte.config.js
+++ svelte.config.js
@@ -8,8 +8,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess(),
-	'javascript.validate.enable': false
+	preprocess: vitePreprocess()
 };
 
 export default config;
