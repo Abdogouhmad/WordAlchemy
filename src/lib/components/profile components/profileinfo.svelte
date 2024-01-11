2d2d415b4b507d52f1eb91ca5780d36f44239f6c
div-styl <gouhmada@gmail.com>
2024-01-11 22:37:15 +0100

    the date of join and last joined is updated

---
+++ src/lib/components/profile components/profileinfo.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let data;
+</script>
+
+<div
+	class="max-w-lg mx-auto dark:border-blue-600/30 border-blue-600/50 border rounded-lg shadow-md p-5">
+	<h1 class="text-left text-base md:text-2xl font-extrabold mt-3">{data.user.fullname}</h1>
+	<h2 class="text-left text-gray-400 text-base md:text-2xl font-light">{data.user.username}</h2>
+	<h2 class="text-left text-sm md:text-base  font-light text-gray-600">
+		Member since: {data.user.createdAt.toDateString()}
+	</h2>
+	<h2 class="text-left text-sm md:text-base  font-light text-gray-600">
+		Last Updated: {data.user.updatedAt.toDateString()}
+	</h2>
+	<h2 class="text-left text-sm font-light text-gray-600">{data.user.verified ? 'Verified' : 'Not Verified'}</h2>
+	<h2 class="text-left text-sm font-light text-gray-600">{data.user.email}</h2>
+	<hr class="border-0 bg-blue-600/50 my-3 h-px dark:bg-white/25 w-full" />
+	<h2 class="font-semibold text-sm md:text-2xl">Bio</h2>
+	<form action="?/bio" method="post" class="pt-3">
+		<input
+			type="text"
+			maxlength="100"
+			class="w-full p-5 bg-white border border-blue-600/30 dark:bg-gray-950 rounded-lg"
+			placeholder="Enter your bio" />
+	</form>
+</div>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
-export { default as Moon } from './components/UI/Moon.svelte';
-export { default as Sun } from './components/UI/Sun.svelte';
 export { default as Navbar } from './components/Header/Header.svelte';
 export { default as DEngine } from './components/Body/Searchbar.svelte';
 export { default as Catlookup } from './components/UI/cat.json';
@@ -8,7 +6,6 @@ export { default as Meaning } from './components/Result/Meaningresult/Meaning.sv
 export { default as Loginform } from './components/forms/logingform.svelte';
 export { default as Registerform } from './components/forms/registerform.svelte';
 export { default as Logoutbutton } from './components/forms/logoutbutton.svelte';
-export { default as Hum } from './components/UI/humberger.svelte';
 export { default as Resetpassword } from './components/forms/resetpassword.svelte';
 export { default as UpdateUser } from './components/forms/updateuser.svelte';
-export { default as Footer } from './components/Footer/Footer.svelte';
+export { default as Userprofile } from './components/profile components/profileinfo.svelte';
