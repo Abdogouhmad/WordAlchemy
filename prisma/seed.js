4f56f7ac2e20e596f806220677449cf2199d548f
div-styl <gouhmada@gmail.com>
2024-01-14 19:55:09 +0100

    warm up the database; new databases are created for category collection :rocket:

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,9 +1,39 @@
 import { db } from '$lib/db';
 
 async function main() {
-	await db.user.deleteMany({});
-	// console.log(`Created user with id: ${user.id}`);
-	console.log('Database seeded!');
+	// await db.category.deleteMany({});
+	// await db.word.deleteMany({});
+	// console.log('Database cleaned');
+	// fill the category and word database
+	//--------------------------------------------------------------------
+	const User = await db.user.findUnique(
+		{ where: { email: 'gouhmada@gmail.com' } }
+	);
+	if (!User) {
+		throw new Error('User not found');
+	}
+
+	// create category
+	const category = await db.category.create({
+		data: {
+			namecategory: 'Education',
+			userId: User.id
+		}
+	});
+	if (!category) {
+		throw new Error('Category couldn\'t be created');
+	}
+
+	// create word
+	const word = await db.word.create({
+		data: {
+			wordtodefine: 'school',
+			categoryId: category.id
+		}
+	});
+	if (!word) {
+		throw new Error('Word couldn\'t be created');
+	}
 }
 
 main()
